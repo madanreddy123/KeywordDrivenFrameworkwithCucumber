70be219f1d671ef433a0caba150de1cb174036a3
Madan <Madanreddy319@gmail.com>
2025-02-16 17:49:40 +0000

    removed test case 2 step def

--- src/main/java/com/stepdefination/Testcase1.java
+++ src/main/java/com/stepdefination/Testcase1.java
@@ -5,57 +5,94 @@ import io.cucumber.java.en.*;
 public class Testcase1 {
 
 
-
-
-
-
-
-
-
-	@Given("While 1 filling the form navigation for Testzen Labs Form to proceed with registration")
+@Given("While 1 filling the form navigation for Testzen Labs Form to proceed with registration")
 public void while_fillingtheformnavigationfortestzenlabsformtoproceedwithregistration() {
 }
 
 
 
-	@When("you correctly entered   the First Name before moving to the next field")
+@When("you correctly entered   the First Name before moving to the next field")
 public void youcorrectlyenteredthefirstnamebeforemovingtothenextfield() {
 }
 
 
 
-	@And("You should carefully  entterred the Last Name so that it matches your official documents")
+@And("You should carefully entterred  the Last Name so that it matches  your official documents")
 public void youshouldcarefullyentterredthelastnamesothatitmatchesyourofficialdocuments() {
 }
 
 
 
-	@And("Before proceeding further make sure to entterring the Phone Number to receive OTP verification")
+@And("Before proceeding further make sure to entterring the Phone Number to receive OTP verification")
 public void beforeproceedingfurthermakesuretoentterringthephonenumbertoreceiveotpverification() {
 }
 
 
 
-	@Then("In the form selection Country from the dropdown list to specify your nationality")
+@Then("In the form selection Country from the dropdown list to specify your nationality")
 public void intheformselectioncountryfromthedropdownlisttospecifyyournationality() {
 }
 
 
 
-	@And("To complete your application kindly upload Resume in the specified format")
+@And("To complete your application kindly upload Resume in the specified format")
 public void tocompleteyourapplicationkindlyuploadresumeinthespecifiedformat() {
 }
 
 
 
-	@Then("checked the Male option if applicable")
+@Then("checked the Male option if applicable")
 public void checkedthemaleoptionifapplicable() {
 }
 
 
 
-	@And("generation a random number for the pin code before submission")
+@And("generation a random number for the pin code before submission")
 public void generationarandomnumberforthepincodebeforesubmission() {
+}
+
+
+
+
+
+	@Given("While filling the form navigate to Testzen Labs Form to proceed with registration")
+public void whilefillingtheformnavigatetotestzenlabsformtoproceedwithregistration() {
+}
+
+
+
+	@When("you correctly enter First Name before moving to the next field")
+public void youcorrectlyenterfirstnamebeforemovingtothenextfield() {
+}
+
+
+
+	@And("You should carefully enter Last Name so that it matches your official documents")
+public void youshouldcarefullyenterlastnamesothatitmatchesyourofficialdocuments() {
+}
+
+
+
+	@And("Before proceeding further make sure to enter Phone Number to receive OTP verification")
+public void beforeproceedingfurthermakesuretoenterphonenumbertoreceiveotpverification() {
+}
+
+
+
+	@Then("In the form select Country from the dropdown list to specify your nationality")
+public void intheformselectcountryfromthedropdownlisttospecifyyournationality() {
+}
+
+
+
+	@Then("check the Male option if applicable")
+public void checkthemaleoptionifapplicable() {
+}
+
+
+
+	@And("generate a random number for the pin code before submission")
+public void generatearandomnumberforthepincodebeforesubmission() {
 }
 
 
